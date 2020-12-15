da35adf109d8048f07892c41b59f22895fdcc120
007rohanpandey <71799045+007rohanpandey@users.noreply.github.com>
2020-12-14 17:59:37 -0700

    LAB4

---
+++ powershell/Lab4.ps1
@@ -0,0 +1,14 @@
+﻿write-output 'PROCESSOR_INFORMATION'
+get-ciminstance -Class Win32_processor
+write-output 'OPERATINGSYSTEM_INFORMATION'
+get-ciminstance -Class Win32_operatingsystem
+write-output 'PROCSSOR_SPECIFIC_INFORMATION'
+get-ciminstance -Class Win32_processor -ErrorAction Stop | Select-Object L1CacheSize, L2CacheSize, L3CacheSize, Number*, CurrentClockSpeed, MaxClockSpeed
+write-output 'RAM_INFORMATION'
+get-ciminstance -Class Win32_physicalmemory -ErrorAction Stop | Select-Object Description, Capacity, BankLabel
+write-output 'NETWORKADAPTERCONFIGURATION_INFORMATION'
+get-ciminstance -class win32_networkadapterconfiguration |
+where-object ipenabled -eq true |
+format-table description, InterfaceIndex, IPAddress, SubnetMask, DNSDomain, DNSServer
+write-output 'VIDEOCARD_INFORMATION'
+get-ciminstance -Class Win32_videocontroller -ErrorAction Stop | Select-Object Description, VideoModeDescription
